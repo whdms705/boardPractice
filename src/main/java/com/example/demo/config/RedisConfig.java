055a771e16757ba1a60ab8ddcd12c4bfcd643d1b
whdms705 <whdms705@nate.com>
2018-07-08 01:19:51 +0900

    first commit

---
+++ src/main/java/com/example/demo/config/RedisConfig.java
@@ -0,0 +1,9 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.JedisPoolConfig;
+
+@Configuration
+public class RedisConfig {
+
+}
