41363e7971c282b46f225edf5d1f5d7b71b16337
whdms705 <whdms705@nate.com>
2018-07-13 00:29:15 +0900

    Update BoardController.java

--- src/main/java/com/example/demo/controller/BoardController.java
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -1,5 +1,21 @@
 package com.example.demo.controller;
 
-public class BoardController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.Board;
+import com.example.demo.repository.BoardRepository;
+
+@RestController
+@RequestMapping("board")
+public class BoardController {
+	@Autowired BoardRepository boardRepository;
+	
+	@RequestMapping("boards")
+	public List<Board> boards(){
+		return boardRepository.findAll();
+	}
 }
