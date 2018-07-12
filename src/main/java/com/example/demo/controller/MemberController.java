72cd335b3a1f8e951ab206d3a470b7e91f594df8
whdms705 <whdms705@nate.com>
2018-07-13 00:29:45 +0900

    Add Test source

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -1,5 +1,37 @@
 package com.example.demo.controller;
 
-public class MemberController {
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.domain.Member;
+import com.example.demo.repository.MemberRepository;
+
+
+@RestController
+@RequestMapping("member")
+public class MemberController {
+@Autowired MemberRepository memberRepository;
+	Log log = LogFactory.getLog(MemberController.class);
+	
+	@GetMapping("/1")
+	public void boards(){
+		Member member = new Member();
+		member.setMember_id("20121201");
+		member.setPassword("qwer123");
+		member.setEmail("eunho@osstem.com");
+		
+		memberRepository.save(member);
+		log.info("member insert");
+	}
+	
+	@GetMapping("/")
+	public void start(){
+		
+		log.info("member start");
+	}
 }
