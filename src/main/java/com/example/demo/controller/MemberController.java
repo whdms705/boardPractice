b9f8a5a2d60096325ec79d578af0d11b0b910bb4
whdms705 <whdms705@nate.com>
2018-07-14 15:58:17 +0900

    Update MemberController.java
    Add save member

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -2,28 +2,30 @@ package com.example.demo.controller;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.Member;
+import com.example.demo.domain.request.MemberRequestDto;
 import com.example.demo.repository.MemberRepository;
 
 
 @RestController
 @RequestMapping("member")
 public class MemberController {
-@Autowired MemberRepository memberRepository;
+	
+	MemberRepository memberRepository;
 	Log log = LogFactory.getLog(MemberController.class);
 	
 	@GetMapping("/1")
 	public void boards(){
-		Member member = new Member();
-		member.setMember_id("20121201");
+		Member member = null;
+		/*member.setMember_id("20121201");
 		member.setPassword("qwer123");
-		member.setEmail("eunho@osstem.com");
+		member.setEmail("eunho@osstem.com");*/
 		
 		memberRepository.save(member);
 		log.info("member insert");
@@ -33,5 +35,10 @@ public class MemberController {
 	public void start(){
 		
 		log.info("member start");
+	}
+	
+	@PostMapping("/")
+	public void saveMember(@RequestBody MemberRequestDto member){
+		memberRepository.save(member.toEntity());
 	}
 }
