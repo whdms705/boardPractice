7e4b3b03cd2d5366ff1469e85e4a389969d1822b
whdms705 <whdms705@nate.com>
2018-07-19 22:39:05 +0900

    Update CommentRepository.java

--- src/main/java/com/example/demo/repository/CommentRepository.java
+++ src/main/java/com/example/demo/repository/CommentRepository.java
@@ -7,7 +7,8 @@ import com.example.demo.domain.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment , Long> {
 
-	//계층형 댓글 위치를 위한 분기값
+	
+	//계층형 댓글 위치를 위한 분기값(jpql로 처리할예정)
 	@Query("")
 	int  commentGubun();
 	
