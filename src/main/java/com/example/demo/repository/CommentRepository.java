e15427ecb581607bc0df516fbdbdf0b2031519ae
JO EUNHO <eunho@osstem.com>
2018-07-19 22:36:51 +0900

    Update CommentRepository.java
    Add jpql

--- src/main/java/com/example/demo/repository/CommentRepository.java
+++ src/main/java/com/example/demo/repository/CommentRepository.java
@@ -1,9 +1,16 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.demo.domain.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment , Long> {
 
+	//계층형 댓글 위치를 위한 분기값
+	@Query("")
+	int  commentGubun();
+	
+
+	
 }
