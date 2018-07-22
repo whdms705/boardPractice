de4a3c250217219bfeeeb78e0dd00656ccc378fe
whdms705 <whdms705@nate.com>
2018-07-23 01:47:11 +0900

    Update CommentRepository.java
    Add jpql

--- src/main/java/com/example/demo/repository/CommentRepository.java
+++ src/main/java/com/example/demo/repository/CommentRepository.java
@@ -7,10 +7,16 @@ import com.example.demo.domain.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment , Long> {
 
-	
-	//계층형 댓글 위치를 위한 분기값(jpql로 처리할예정)
-	@Query("")
-	int  commentGubun();
+	/*SELECT NVL(MIN(SORTS),0) FROM BOARD
+	   WHERE  BGROUP = (원글의 BGROUP)
+	   AND SORTS > (원글의 SORTS)
+	   AND DEPTH <= (원글의 DEPTH)
+
+
+	출처: http://vkein.tistory.com/entry/답변형-게시판 [Vkein Programer]
+*/	//계층형 댓글 위치를 위한 분기값(jpql로 처리할예정)
+	@Query("SELECT COALESCE(MIN(c.sorts),0) FROM Comment c WHERE c.cgroup = ?1 AND c.sorts > ?2 AND c.depth <= ?3")
+	Integer  commentGubun(int cgroup,int sorts ,int depth);
 	
 
 	
