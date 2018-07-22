5148e32f05b6986c10df1c06c0cb0d666793b2bf
whdms705 <whdms705@nate.com>
2018-07-23 01:45:39 +0900

    Update CommentController.java
    Add insertComment

--- src/main/java/com/example/demo/controller/CommentController.java
+++ src/main/java/com/example/demo/controller/CommentController.java
@@ -2,19 +2,30 @@ package com.example.demo.controller;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.request.CommentRequestDto;
+import com.example.demo.repository.CommentRepository;
 import com.example.demo.service.CommentService;
 
+@RestController
+@RequestMapping("comment")
 public class CommentController {
 
 	Log log = LogFactory.getLog(CommentController.class);
 	CommentService commentService;
+	@Autowired CommentRepository commentRepository;
 	
+	@PostMapping("/")
 	public void insertComment(@RequestBody CommentRequestDto comment){
 		log.info("insert start comment");
-		commentService.insertComment(comment);
+		//commentService.insertComment(comment);
+		int gubun = commentRepository.commentGubun(comment.getCgroup(),comment.getSorts(),comment.getDepth());
+		log.info("gubun value : "+gubun);
 		log.info("insert end comment");
 	}
 }
