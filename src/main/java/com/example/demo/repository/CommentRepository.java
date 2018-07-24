d60e6637d5d32b9fd8c25b0433247c87c43575ca
whdms705 <whdms705@nate.com>
2018-07-25 01:24:46 +0900

    Update CommentRepository
    Add repository

--- src/main/java/com/example/demo/repository/CommentRepository.java
+++ src/main/java/com/example/demo/repository/CommentRepository.java
@@ -11,13 +11,21 @@ public interface CommentRepository extends JpaRepository<Comment , Long> {
 	   WHERE  BGROUP = (원글의 BGROUP)
 	   AND SORTS > (원글의 SORTS)
 	   AND DEPTH <= (원글의 DEPTH)
-
-
-	출처: http://vkein.tistory.com/entry/답변형-게시판 [Vkein Programer]
+	   
 */	//계층형 댓글 위치를 위한 분기값(jpql로 처리할예정)
 	@Query("SELECT COALESCE(MIN(c.sorts),0) FROM Comment c WHERE c.cgroup = ?1 AND c.sorts > ?2 AND c.depth <= ?3")
 	Integer  commentGubun(int cgroup,int sorts ,int depth);
 	
+	/*3. SELECT NVL(MAX(SORTS),0) + 1 FROM BOARD 
+    WHERE BGROUP = (원글의 BGROUP);*/
+	
+	@Query("SELECT COALESCE(MIN(c.sorts),0)+1 FROM Comment c where c.cgroup = ?1")
+	Integer sortsValue(int cgroup);
+	
+	/*UPDATE BOARD SET SORTS = SORTS + 1 
+			  WHERE BGROUP =  (원글의 BGROUP)  AND SORTS >= (1번값)*/
 
+	@Query("UPDATE Comment c SET c.sorts = c.sorts + 1 WHERE c.cgroup = ?1 AND c.sorts >=?2 ")
+	void updateSorts(int cgroup,int gubun);
 	
 }
